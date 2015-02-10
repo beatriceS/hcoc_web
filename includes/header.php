4e298ed495bacd7576fc68058390b09fb0823f0d
Beatrice <beatrice.salim@stu.aii.edu>
2015-02-10 15:27:53 -0800

    init

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<div id="header">
+	<div id="logo">
+	<img src="../images/logo.png">
+	</div>
+
+	<div id="tagline">
+    <p> Love Strong • Believe Strong • Forgive Strong </p>
+    </div>
+
+	<div id="nav">
+    <ul>
+    	<li> <a href="index.html"> Home </a> </li>
+        <li> <a href="about.html"> About </a> </li>
+        <li> <a href="media.html"> Media </a> </li>
+        <li> <a href="events.html"> Events </a> </li>
+        <li> <a href="ministries.html"> Ministries </a> </li>
+        <li> <a href="give.html"> Give </a> </li>
+        <li> <a href="contact.html"> Contact </a> </li>
+    </ul>
+    </div>
+</div> <!--closing id header-->
